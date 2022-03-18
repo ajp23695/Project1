2c8be3a369199bfcb32166dccde9c8bbc719f065
Aniket Patel <ajp23695@gmail.com>
2022-03-18 18:13:18 +0000

    added

--- sum.py
+++ sum.py
@@ -2,5 +2,6 @@ first_value = int(input('Enter the first value :- '))
 second_value = int(input('Enter the Second Value :- '))
 
 total = first_value + second_value
-
+mul = first_value * second_value
 print(total)
+print(mul)
